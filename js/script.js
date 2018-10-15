cf8728438bd1acadf31dd609063cc408675e781d
Justin <justin@baal-software.com>
2018-10-15 16:53:14 -0600

    Add functions for event listeners

--- js/script.js
+++ js/script.js
@@ -1,39 +1,110 @@
 $("document").ready(function () {
+  //Min value
+  const MIN_VALUE = 0;
+  //Maximum guess value
   let maxVal = 100;
-  const numGen = function (maxVal){
-    return Math.floor(Math.random() * maxVal + 1);
+  //Function to call new value after reset
+  const numGen = function (i){
+    return Math.floor(Math.random() * i + 1);
   }
+  
+  
 
-  let ranNum = numGen();
+  //The current max value to use in logic
+  let ranNum = numGen(maxVal);
+  console.log(ranNum);
+  //Hides Elements from view
+  $("#last-guess").css("visibility", "hidden");
+  $("#high-low-boom").css("visibility", "hidden");
 
-  $("#guess").submit((e) => {
+  //Regex for checking non numerals
+  let regex = /^[^\W]+$/;
 
+  //Checks number validity, checks after every new char
+  $("#input-box").on('input', () => {
+    $("header").removeClass('error');
+    /*
+    If the input is greater, lower, or wrong resets input 
+    and shows error message.
+    */
+    if($("#input-box").val() > maxVal ||
+      $("#input-box").val() < MIN_VALUE || 
+      !regex.test($("#input-box").val())){
+
+        //Adds the class the shows error here
+      $("header").addClass('error');
+        //Clears input here
+        $("#input-box").val(""); 
+      }
+    })
+
+  //Grabs the event in case I want to manipulate it later
+  $("#guess-button").click((e) => {
+
+    //Copies the value from input-box
     let myGuessNum = $("#input-box").val();
-    // $("#input-box").val("");
-    if(myGuessNum > ranNum){
+
+    $("#last-guess").css("visibility", "visible");
+    $("#high-low-boom").css("visibility", "visible");
+    
+    //Conditional checks
+    if(parseInt(myGuessNum) > ranNum){
       $("#high-low-boom").text("That is too high");
     }
-    else if(myGuessNum < ranNum){
+    else if(parseInt(myGuessNum) < ranNum){
       $("#high-low-boom").text("That is too low");
     }
     else{
       $("#high-low-boom").text("BOOM!");
     }
 
-
+    //Displays what the last guess was
     $("#last-guess").text(myGuessNum);
+
+    /*
+    I have nowhere to send the data so I prevent the default
+    behaviour of it wanting to refresh mah page.
+    */
+    e.preventDefault();
   });
 
-  $("#clear").click(() => $("#input-box").val(""));
-  $("#last-guess").click(() => alert("last-guess"));
-  
-  $("#reset").click(() => {
-    $("#input-box").val("");
-    ranNum = numGen();
-    $("#last-guess").text("Loading...");
-    $("#high-low-boom").text("Awaiting Guess...");
+  //Logic for the clear button, initial state disabled
+  $("#clear-button").prop("disabled", true);
+  /*
+    Disables the clear button if there is nothing in the inputbox.
+    Note: Works with keyup only.
+    */
+    $("#input-box").change(function () {
+      if($(this).val().length != 0){
+        $("#clear-button").prop("disabled", false);
+      }
+      else{
+        $("#clear-button").prop("disabled", true);
+      }
+    })
+
+  /*
+    Even having the above code fire onchange I need to use
+    the click handler to check again to disable the clear 
+    button. Look into further if get time.
+    */
+    $("#clear-button").click(() => {
+      $("#input-box").val("");
+      if($(this).val().length != 0){
+        $("#clear-button").prop("disabled", false);
+      }
+      else{
+        $("#clear-button").prop("disabled", true);
+      }
+    });
+
+    $("#reset").click(() => {
+      $("#input-box").val("");
+      ranNum = numGen();
+      $("#last-guess").css("visibility", "hidden");
+      $("#high-low-boom").css("visibility", "hidden");
+    });
+
+    //Timer for checking reset needs
 
   });
-
-});
-
